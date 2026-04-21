7df6e503bb6f1929ef66bfbfb5273b3fd94ed109
Neguim6 <solosagradot@gmail.com>
2026-04-20 23:37:53 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('fin-v1').then((cache) => cache.addAll(['index.html', 'script.js'])));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
