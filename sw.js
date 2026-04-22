ea088a92e951c0bb93f85439b80488c8babee1e2
Neguim6 <solosagradot@gmail.com>
2026-04-21 23:30:45 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,48 @@
+const CACHE_NAME = 'finnotes-v51';
+const ASSETS = [
+  'index.html',
+  'style.css',
+  'script.js',
+  'manifest.json'
+];
+
+// Instalação: Salva os arquivos essenciais no cache
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('fin-v51').then((cache) => cache.addAll(['index.html', 'script.js'])));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('SW: Cacheando arquivos essenciais');
+      return cache.addAll(ASSETS);
+    })
+  );
+  // Força o SW a se tornar ativo imediatamente
+  self.skipWaiting();
 });
 
+// Ativação: Remove caches de versões anteriores
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Interceptação: Estratégia Stale-While-Revalidate
+// Tenta carregar do cache para ser instantâneo, mas atualiza o cache em background
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      const fetchPromise = fetch(e.request).then((networkResponse) => {
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, networkResponse.clone());
+        });
+        return networkResponse;
+      });
+      return cachedResponse || fetchPromise;
+    })
+  );
 });
