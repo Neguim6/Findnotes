582bd314f7d099866746f5f2e0aa3978da27453a
Neguim6 <solosagradot@gmail.com>
2026-04-21 21:40:48 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,17 @@
-const CACHE_NAME = 'finnotes-v14';
-const ASSETS = ['./index.html', './script.js', './manifest.json'];
+const CACHE_NAME = 'finnotes-v16';
+const ASSETS = ['./', './index.html', './script.js', './manifest.json'];
 
 self.addEventListener('install', (e) => {
+    self.skipWaiting();
     e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+        caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
     );
 });
 
 self.addEventListener('activate', (e) => {
     e.waitUntil(
-        caches.keys().then((keys) =>
-            Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+        caches.keys().then(keys => 
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
         )
     );
     self.clients.claim();
@@ -20,18 +21,11 @@ self.addEventListener('fetch', (e) => {
     if (e.request.method !== 'GET') return;
     e.respondWith(
         fetch(e.request)
-            .then((res) => {
+            .then(res => {
                 const clone = res.clone();
-                caches.open(CACHE_NAME).then((c) => c.put(e.request, clone));
+                caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
                 return res;
             })
             .catch(() => caches.match(e.request))
     );
-});
-
-// Lógica de Notificação enviada pelo Script principal
-self.addEventListener('message', (e) => {
-    if (e.data && e.data.type === 'CHECK_ALERTS') {
-        // Lógica de envio de notificação push aqui
-    }
 });
