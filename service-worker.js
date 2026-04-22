058977cac1892ecbf44013724be557f02571d41c
Neguim6 <solosagradot@gmail.com>
2026-04-21 21:43:43 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,21 @@
-const CACHE_NAME = 'finnotes-v16';
+const CACHE_NAME = 'finnotes-v20';
 const ASSETS = ['./', './index.html', './script.js', './manifest.json'];
 
 self.addEventListener('install', (e) => {
     self.skipWaiting();
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
-    );
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', (e) => {
-    e.waitUntil(
-        caches.keys().then(keys => 
-            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-        )
-    );
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
     self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
     if (e.request.method !== 'GET') return;
-    e.respondWith(
-        fetch(e.request)
-            .then(res => {
-                const clone = res.clone();
-                caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
-                return res;
-            })
-            .catch(() => caches.match(e.request))
-    );
+    e.respondWith(fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        return res;
+    }).catch(() => caches.match(e.request)));
 });
